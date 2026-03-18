f1685c6e144bcee5f32a0869f81259b7368cb5e3
Om Pandey <ompandey628782@gmail.com>
2026-03-19 01:27:45 +0530

    frontend-set-backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.PROD 
-  ? "https://civiclens-5.onrender.com/api" 
-  : "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
